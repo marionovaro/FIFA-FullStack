b7aa30781b0e2f57c4fda0a34ad623acb72b4600
marionovaro <marionovaro04@gmail.com>
2023-11-21 18:15:22 +0100

    FIFA Full-Stack

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -6,24 +6,52 @@ const AuthContext = createContext(); //? se pone vacío porque lo usa el context
 export const AuthContextProvider = ({ children }) => { //? va a grapear a otros elementos por eso ponemos el children
 
   const [user, setUser] = useState(() => {
-    const user = localStorage.getItem("user") //? miramos si en el localstorage hay usuario
-    return user ? JSON.parse(user) : null //? si lo hay, devolvemos ese usario parseado, y si no, pues nada
+    const user = localStorage.getItem("user") //? ---- miramos si en el localstorage hay usuario
+    return user ? JSON.parse(user) : null //? -------- si lo hay, devolvemos ese usario parseado, y si no, pues nada
   });
 
+  //! ESTADO ALLUSER (guardamos la info )
+  const [allUser, setAllUser] = useState({
+    data: {
+      confirmationCode: "", //? esto lo va a necesitar el siguente paso de la web, que es el check Code
+      user: { //? devolvemos el usuario pero solo lo que necesitamos
+        password: "",
+        email: "",
+      },
+    },
+  });
+
+  //! FUNCIÓN BRIGE ASINCRONÍA ()
+  const bridgeData = (state) => {
+    const data = localStorage.getItem("data") //? coge los datos del register que hemos metido en el localstorage temporalmente
+    const dataJson = JSON.parse(data) //? ------- los parseamos a JS
+    switch (state) {
+      case "ALLUSER": 
+        setAllUser(dataJson); //? seteamos allUser con data (recordemos que la hemos sacado del register)
+        localStorage.removeItem("data");
+        
+        break;
+    
+      default:
+        break;
+    }
+  }
+
+
   const login = (data) => {
-    localStorage.setItem("user", data) //? podemos meter la data porque la hemos recibido en string, en el user, metemos la data
-    const parseUser = JSON.parse(data) //? la inversa (string ==> no string) lo hacemos para meterlo en el estado
-    setUser(parseUser) //? aquí lo metemos en el estado
+    localStorage.setItem("user", data) //? ---- podemos meter la data porque la hemos recibido en string, en el user, metemos la data
+    const parseUser = JSON.parse(data) //? ---- la inversa (string ==> no string) lo hacemos para meterlo en el estado
+    setUser(parseUser) //? -------------------- aquí lo metemos en el estado
   }
 
   const logout = () => {
-    localStorage.removeItem("user") //? elimina el user del localstorage
-    setUser(null) //? como no hay user, el estado user se queda vacío
+    localStorage.removeItem("user") //? ---- elimina el user del localstorage
+    setUser(null) //? ---------------------- como no hay user, el estado user se queda vacío
   }
 
-  const value = useMemo(() => ({ //? memoriza los datos, que lo que hace es un hook memoriza los returns de las funciones
-    user, setUser, login, logout //? qué memoriza
-  }), [user]) //? array de dependencias para que cada vez que cambie el usuario vuelva a memorizar
+  const value = useMemo(() => ({ //? ---- memoriza los datos, que lo que hace es un hook memoriza los returns de las funciones
+    user, setUser, login, logout //? ---- qué memoriza
+  }), [user]) //? ----------------------- array de dependencias para que cada vez que cambie el usuario vuelva a memorizar
 
   return <AuthContext.Provider value = {value}>{children}</AuthContext.Provider>
 }
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
+import { useForm } from "react-hook-form";
+import "./Register.css";
+import { useEffect, useState } from "react";
+import { Uploadfile } from "../components"; 
+
+import { Link, Navigate } from "react-router-dom";
+import { useAuth } from "../context/authContext";
+import { registerUser } from "../services/user.service";
 
 export const Register = () => {
-  return (
-    <div>Register</div>
-  )
+  
+  //! ---- Destructuring ----
+  const { allUser, setAllUser, bridgeData } = useAuth();
+  const { register, handleSubmit } = useForm(); //? nos los da react-router-dom
+
+  //! ---- Estados ----
+  const [res, setRes] = useState({}); //? --------------------- respuesta
+  const [send, setSend] = useState(false); //? ---------------- se está enviando (cargando)
+  const [okRegister, setOkRegister] = useState(false); //? ---- ha salido bien o no?
+
+  return 
 }
